8b4f18f1fbba68cf6e0df5c21a9cc57b0d504106
snipspin <photonenimpuls@gmail.com>
2020-03-28 00:26:25 -0700

    serve with express

---
+++ app.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.static(path.join(__dirname, 'dist')));
+const port = process.env.PORT || '8080';
+app.listen(port, () => { console.log(`Server is running on port ${port}`);})
